194692fbd5492d3a339d2dde78d12d8598ab1ef0
Juri Heins <jheins@hs-mittweida.de>
2023-04-23 19:30:06 +0200

    Added todo crud

---
+++ Website/Data/TodoItem.cs
@@ -0,0 +1,9 @@
+﻿namespace Website.Data
+{
+    public class TodoItem
+    {
+        public string? Title { get; set; }
+        public bool IsDone { get; set; }
+        public bool IsEditable { get; set; }
+    }
+}
